53faa26ee84280392c00484f449dfe17c5188223
ilfa <mhmdilfaj@gmail.com>
2022-01-26 07:24:19 +0700

    Update function.js

--- assets/function.js
+++ assets/function.js
@@ -1,7 +1,14 @@
 //updated selectors imagesloaded
-$('#masonry').imagesLoaded(function() {
-    $('#masonry').masonry({
-        itemSelector: '.masonry-item'
-    });
+$(function(){
+	/* 瀑布流區塊div */
+	var $container = $('.masonry');
+
+	//當圖片讀取完畢才執行
+	$container.imagesLoaded(function (){
+		//選擇瀑布流的區塊名稱
+		$container.masonry({
+			itemSelector: '.masonry-item'
+		})
+	});
 });
       
